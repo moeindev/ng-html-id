8d1fbf6d8e2e50ff1559c26ab298d196948f51cc
mohammad moein abdi <moeinabdi77@gmail.com>
2023-09-09 00:11:39 +0330

    fix log not showing

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ const command = program
 
 let configurationPath = command.getOptionValue(COMMANDER_CONST.commands[0].command);
 if (!configurationPath) {
-    command.error(COMMANDER_CONST.commands[0].error);
     console.info(COMMANDER_CONST.commands[0].error);
     configurationPath = './default-configuration.json';
 }
@@ -22,7 +21,7 @@ if (!configurationPath) {
 if (ConfigUtil.isConfigurationFileExists(configurationPath)) {
     console.info('Configuration found, proceeding...');
 } else {
-    console.error('Configuration not found! exiting');
+    console.info('Configuration not found! exiting');
     process.exit();
 }
 
