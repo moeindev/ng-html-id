d642bed6f36e556f07544f166181e5a790600f12
mohammad moein abdi <moeinabdi77@gmail.com>
2023-09-09 21:47:48 +0330

    feat(implement-logic): implement logic

--- src/add-meta.ts
+++ src/add-meta.ts
@@ -1,12 +1,32 @@
 import {FileUtil} from "./utils/file.util";
 import CLIInfinityProgress from "cli-infinity-progress";
+import {ConfigUtil} from "./utils/config.util";
+import {ComponentModel} from "./model/component.model";
+import {HtmlUtil} from "./utils/html.util";
 
-export function addHtmlIds(progress: CLIInfinityProgress) {
+
+export function addHtmlIds(configurationPath: string, progress: CLIInfinityProgress) {
+    const components = FileUtil.findComponentFiles('src');
+    showComponentsInfo(components);
+    progress.remove();
+    const config = ConfigUtil.getConfiguration(configurationPath);
+    if (components && components.length > 0) {
+        components.forEach(component => {
+            HtmlUtil.addIdsToElements(component, config);
+        });
+    } else {
+        console.info('\n')
+        console.info('No components found, closing');
+        process.exit();
+    }
+}
+
+
+export function showComponentsInfo(components: ComponentModel[]) {
     setTimeout(() => {
         console.info('\n');
         console.info('\n');
         console.info('Here is the list of components:');
-        console.table(FileUtil.findComponentFiles('src'));
-    }, 2000)
-    progress.remove();
+        console.table(components);
+    }, 2000);
 }
